b4877aee884d927b674c0062f47bf7e830ea43f2
Rajanishetty18 <shettyrajani1245@gmail.com>
2025-11-05 12:17:34 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu()
+{
+    const menu=document.querySelector(".menu_link");
+    const icon=document.querySelector(".hamburger_icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
