539330804c72aa51a0445e31d7943fe63b91dcf2
Rajanishetty18 <shettyrajani1245@gmail.com>
2025-11-05 14:51:28 +0530

    Fixed gaps

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function toggleMenu()
     const icon=document.querySelector(".hamburger_icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    setTimeout(() => {}, 50);
 
 }
